8763b4d30960197c85111de80865c5141213d8ec
waghmaresayali24 <waghmaresayali24@gmail.com>
2026-05-23 16:30:05 +0530

    Add files via upload

---
+++ Assignment 1/Program_2.c
@@ -0,0 +1,21 @@
+
+// Program to print marvellous 5 times on the screen
+
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+
+    for(i=0 ; i<=4; i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ Assignment 1/Program_3.c
@@ -0,0 +1,24 @@
+//Program to print 5 to 1
+//error in the question itself 
+#include<stdio.h>
+
+void Display()
+{
+    
+    int i=0;
+    i=5;
+
+    while(i<=5);
+    {
+        printf("%d",i);
+        i++;
+    }
+}
+
+
+int main()
+{
+    Display();
+
+    return 0;
+}
